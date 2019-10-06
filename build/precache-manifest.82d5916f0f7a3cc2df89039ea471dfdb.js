f290e919202965c5989e15d5ef3720e82d037433
Андрей Рогацкий <cactus@bk.ru>
2019-10-06 22:55:27 +0300

    some fix

---
+++ build/precache-manifest.82d5916f0f7a3cc2df89039ea471dfdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8d4c8fdd2f82137b588d7feca34905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b58a72134b166a8f24b",
+    "url": "/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "7f8881efecc39ab5c4ba",
+    "url": "/static/js/2.b4944e9a.chunk.js"
+  },
+  {
+    "revision": "7b58a72134b166a8f24b",
+    "url": "/static/js/main.de4403b2.chunk.js"
+  },
+  {
+    "revision": "844f2d010548b6594ac6",
+    "url": "/static/js/runtime-main.7a70ba2c.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/static/media/logo.a55f1c43.svg"
+  }
+]);
