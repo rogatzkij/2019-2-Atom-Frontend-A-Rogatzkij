18b37543d9eacffb11c6097d53c42f4a9f009787
Андрей Рогацкий <cactus@bk.ru>
2019-10-06 19:44:30 +0300

    save message at Local Storage

--- web-components/src/components/MessageForm.js
+++ web-components/src/components/MessageForm.js
@@ -30,11 +30,58 @@ class MessageForm extends HTMLElement {
 
     this.$form.addEventListener('submit', this.onSubmit.bind(this));
     this.$form.addEventListener('keypress', this.onKeyPress.bind(this));
+
+    for (let i = 0; i < localStorage.length; i += 1) {
+      const key = localStorage.key(i);
+      try {
+        const obj = JSON.parse(localStorage.getItem(key));
+        const time = Date(key);
+
+        const msgField = document.createElement('div');
+        msgField.className = 'message-field';
+        this.$message.appendChild(msgField);
+
+        const msgText = document.createElement('div');
+        msgText.className = 'message-field-text';
+        msgText.innerText = obj.text;
+        msgField.appendChild(msgText);
+
+        const msgTime = document.createElement('div');
+        msgTime.className = 'message-field-time';
+        msgTime.innerText = time.toString();
+        msgField.appendChild(msgTime);
+
+        // alert(`${key}: ${obj.text}`);
+      } catch {
+        console.log('=(');
+      }
+    }
   }
 
   onSubmit(event) {
     event.preventDefault();
-    this.$message.innerText = this.$input.value;
+
+    const now = Date.now();
+
+    const msgField = document.createElement('div');
+    msgField.className = 'message-field';
+    this.$message.appendChild(msgField);
+
+    const msgText = document.createElement('div');
+    msgText.className = 'message-field-text';
+    msgText.innerText = this.$input.value;
+    msgField.appendChild(msgText);
+
+    const msgTime = document.createElement('div');
+    msgTime.className = 'message-field-time';
+    msgTime.innerText = now.toString();
+    msgField.appendChild(msgTime);
+
+    const msgData = {};
+    msgData.text = this.$input.value;
+    msgData.author = 'me';
+
+    localStorage.setItem(now.toString(), JSON.stringify(msgData));
   }
 
   onKeyPress(event) {
