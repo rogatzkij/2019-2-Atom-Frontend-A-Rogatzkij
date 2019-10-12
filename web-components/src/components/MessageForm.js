3e11676c6f0a1710c9b9f182220711193134b5af
Андрей Рогацкий <cactus@bk.ru>
2019-10-12 22:49:53 +0300

    chat with style

--- web-components/src/components/MessageForm.js
+++ web-components/src/components/MessageForm.js
@@ -1,47 +1,151 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
-        form-input {
-            width: 100%;
-        }
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap&subset=cyrillic');
 
-        input[type=submit] {
-            visibility: collapse;
-        }
-
-        .result{
-          display: flex;
-          flex-direction: column;
-        }
-
-        .left{
-          display: flex;
-          justify-content: flex-end;
-        }
-
-        .right{
-          display: flex;
-          justify-content: flex-start;
-        }
-
-        .message-field{
-          min-width: 20%;
-          border-style: 1px solid rgb(223, 190, 255);
-          border-radius: 8px;
-
-          background-color: rgb(223, 190, 255);
-        }
-
-        .message-input {
-          display: flex;
-          flex-direction: column;
-        }
-
+    body{
+        margin: 0px;
+        font-family: 'Roboto', sans-serif;
+    }
+    
+    .page{
+        display: flex;
+        flex-direction: column;
+        background-color: darkgreen;
+    
+        width: 100vw;
+        height: 100vh;
+    }
+    
+    .header{
+        display: flex;
+        flex-grow: 0;
+        height: 80px;
+        background-color: #8d24aa;
+    }
+    
+    .content{
+        display: flex;
+        flex-grow: 1;
+        overflow-y: scroll;
+        flex-direction: column-reverse;
+        height: 100vh;  
+        background-color: #f8f8f8;
+    }
+    
+    .content::-webkit-scrollbar{
+        display: none;
+    }
+    
+    .message-box{
+        display: flex;
+        flex-direction: column;
+    
+        padding-bottom: 5px;
+        justify-content: flex-end;
+        align-items: flex-start;
+        width: 100%;
+    }
+    
+    /* Сообщение */
+    .message{
+        display: flex;
+        flex-direction: column;
+        
+        max-width: 60%;
+        
+        margin: 5px 10px 5px 10px;
+        padding: 10px;
+        background-color: #f3e6f5;
+    
+        border-radius: 4px;
+        border: 1px solid #e7dce9;
+        box-shadow: 0 0 5px #c2bcc37e;
+    }
+    
+    .message > p{
+        font-size: 20px;
+    }
+    
+    .author{
+        align-self: flex-end;
+        background-color: #fff;
+    }
+    
+    /* Текст сообщения */
+    .message-content{
+        /* Пренос слов */
+        overflow-wrap: break-word;
+    
+        display: block;
+    }
+    
+    /* Блок с временем и галочкой */
+    .message-meta{
+        display: flex;
+        justify-content: flex-end;
+        align-items: baseline;
+    }
+    
+    /* Время сообщения */
+    .meta-time{
+        display: inline;
+        font-size: 14px;
+    
+        color: #918791;
+    }
+    
+    /* Галочка у сообщения */
+    .meta-status{
+        display: none;
+    
+        margin-left: 5px;
+        width: 10px;
+        height: 10px;   
+    }
+    
+    .author > .message-meta > .meta-status{
+        display: inline-block;
+    
+        background: url(img/check.png) 100% 100% no-repeat; /* Добавляем фон */
+        background-size: cover; /* Масштабируем фон */  
+    }
+    
+    .readen > .message-meta > .meta-status{
+        background: url(img/double_check.png) 100% 100% no-repeat; /* Добавляем фон */
+        background-size: cover; /* Масштабируем фон */
+    }
+    
+    
+    .footer{
+        display: flex;
+        flex-grow: 0;
+        height: 50px;
+    
+        background-color: coral;
+    }
     </style>
     <form>
-        <div class="result"></div>
-        <div class="message-input">
-          <form-input name="message-text" placeholder="Сообщение"></form-input>
+        <div class="page">
+            <div class="header">head</div>
+            <div class="content">
+                <div class="message-box">
+
+                    <div class="message">
+                        <span class="message-content">Бла бла</span>
+                        <div class="message-meta">
+                            <span class="meta-time">12:31</span>
+                            <div class="meta-status"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="footer">
+                <div class="message-input">
+                    <form-input name="message-text" placeholder="Сообщение"></form-input>
+                </div>
+            </div>
         </div>
     </form>
 `;
@@ -51,9 +155,10 @@ class MessageForm extends HTMLElement {
     super();
     this.shadowRoot = this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
     this.$form = this.shadowRoot.querySelector('form');
     this.$input = this.shadowRoot.querySelector('form-input');
-    this.$message = this.shadowRoot.querySelector('.result');
+    this.$message = this.shadowRoot.querySelector('.message-box');
 
     this.$form.addEventListener('submit', this.onSubmit.bind(this));
     this.$form.addEventListener('keypress', this.onKeyPress.bind(this));
@@ -90,28 +195,32 @@ class MessageForm extends HTMLElement {
   }
 
   // отрисовка сообщения на странице
-  createMessage(text, time, left) {
-    const msgPosition = document.createElement('div');
-    if (left === true) {
-      msgPosition.className = 'left';
-    } else {
-      msgPosition.className = 'right';
+  createMessage(text, time, isAuthor) {
+    const msg = document.createElement('div');
+    msg.classList.add('message');
+    if (isAuthor) {
+      msg.classList.add('author');
     }
-    this.$message.appendChild(msgPosition);
+    this.$message.appendChild(msg);
 
-    const msgField = document.createElement('div');
-    msgField.className = 'message-field';
-    msgPosition.appendChild(msgField);
-
-    const msgText = document.createElement('div');
-    msgText.className = 'message-field-text';
+    const msgText = document.createElement('span');
+    msgText.classList.add('message-content');
     msgText.innerText = text;
-    msgField.appendChild(msgText);
+    msg.appendChild(msgText);
 
-    const msgTime = document.createElement('div');
-    msgTime.className = 'message-field-time';
-    msgTime.innerText = time;
-    msgField.appendChild(msgTime);
+    const msgMeta = document.createElement('div');
+    msgMeta.classList.add('message-meta');
+
+    const metaTime = document.createElement('span');
+    metaTime.classList.add('meta-time');
+    metaTime.innerText = time;
+    msgMeta.appendChild(metaTime);
+
+    const metaStatus = document.createElement('div');
+    metaStatus.classList.add('meta-status');
+    msgMeta.appendChild(metaStatus);
+
+    msg.appendChild(msgMeta);
   }
 
   onKeyPress(event) {
